52a54c86c739e1113adca27f9cd3a0fa267726fa
90531 <90531@DESKTOP-LI1OTC9>
2020-10-31 23:45:35 +0300

    Proje dosyası ekle.

---
+++ Factoriyel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factoriali alınacak sayıyı giriniz.");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            if (sayi != 0)
+            {
+                for (int i = 1; i <= sayi; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine(factorial);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Odev-Palindrom_ve_Faktoriyel/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_Palindrom_ve_Faktoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kelime;
+            string devamKontrol;
+            do
+            {
+                Console.WriteLine("Kelimeyi giriniz.");
+                kelime = Console.ReadLine();
+                while (kelime.Length == 1 || kelime.Length == 2)
+                {
+                    Console.WriteLine("Lütfen kelimeyi giriniz!");
+                    kelime = Console.ReadLine();
+                }
+                if (kelime.Equals(PalindromKontrol(kelime)))
+                {
+                    Console.WriteLine("Girdiğiniz kelime palindromdur.");
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz kelime palindrom değildir.");
+                }
+                Console.WriteLine("Tekrar palindrom sorgulaması yapmak istermisin? evet:e hayı:h");
+                devamKontrol = Console.ReadLine();
+                while (!(devamKontrol == "e" || devamKontrol == "h"))
+                {
+                    Console.WriteLine("Tekrar palindrom sorgulaması yapmak istermisin? evet:e hayı:h");
+                    devamKontrol = Console.ReadLine();
+                }
+
+
+            } while (devamKontrol == "e");
+
+
+            Console.ReadKey();
+        }
+        static string PalindromKontrol(string kelime)
+        {
+            string palindrom = "";
+            for (int i = kelime.Length - 1; i >= 0; i--)
+            {
+                palindrom += kelime[i];
+            }
+            return palindrom;
+        }
+    }
+}
